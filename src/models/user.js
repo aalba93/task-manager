d642fab080277a3c465e6f8f0623e24783641354
Andres Alba <andres_aalba@hotmail.com>
2019-04-02 16:39:39 -0500

    Refactor user model to hide private data (Lecture 112)

--- src/models/user.js
+++ src/models/user.js
@@ -71,6 +71,14 @@ userSchema.methods.generateAuthToken = async function () {
     return token;
 };
 
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+    delete userObject.password;
+    delete userObject.tokens;
+    return userObject;
+};
+
 // Hash the plain text password
 userSchema.pre('save', async function (next) {
     const user = this;
