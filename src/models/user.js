f6939629c4b44a46e9781767ad9bc921ed20f7ae
Andres Alba <andres_aalba@hotmail.com>
2019-04-01 22:48:14 -0500

    Add auth token generation function to user model (Lecture 108)

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,9 +40,16 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number');
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
+// Model methods
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({email});
     if (!user) {
@@ -52,6 +60,15 @@ userSchema.statics.findByCredentials = async (email, password) => {
         throw new Error('Unable to login');
     }
     return user;
+};
+
+// Instance metthods
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({_id: user._id.toString()}, 'thisismynewcourse');
+    user.tokens = user.tokens.concat({token});
+    await user.save();
+    return token;
 };
 
 // Hash the plain text password
