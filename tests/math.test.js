0c82d0edcbd001d58295c90fe72e97a441bd5683
Andres Alba <andres_aalba@hotmail.com>
2019-04-04 15:14:07 -0500

    Add real-life functions testing using expect library (Lectures 138 & 139)

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,7 +1,24 @@
-test('Hello world', () => {
+const {calculateTip, celsiusToFahrenheit, fahrenheitToCelsius} = require('../src/math');
 
+test('Should calculate total with tip', () => {
+    const total = calculateTip(10, 0.3);
+    expect(total).toBe(13);
+    // if (total !== 13) {
+    //     throw new Error('Total tip should be 13. Got ' + total);
+    // }
 });
 
-test('This should fail', () => {
-    throw new Error('Failure!');
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10);
+    expect(total).toBe(12.5);
+});
+
+test('Should convert 32 F to 0 C', () => {
+    const temp = fahrenheitToCelsius(32)
+    expect(temp).toBe(0);
+});
+
+test('Should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32);
 });
