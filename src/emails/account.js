0b99da43be2fa1ea8542a15bd4cb078803576db2
Andres Alba <andres_aalba@hotmail.com>
2019-04-04 11:54:11 -0500

    Add user creation and cancelation send email functions (Lecture 132)

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,9 +4,27 @@ const sendgridAPIKey = 'SG.ApY4i0NyRmKplkTyQarLVQ.kMsaGr9Fg6K7FLBgEbiJXYCcJ6jxnr
 
 sgMail.setApiKey(sendgridAPIKey);
 
-sgMail.send({
-    to: 'andres_aalba@hotmail.com',
-    from: 'andres.alba@sendasvalue.com',
-    subject: 'This is my first creation',
-    text: 'I hope this one actually get to you'
-});
+const fromEmail = 'andres_aalba@hotmail.com';
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: fromEmail,
+        subject: 'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+    });
+};
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: fromEmail,
+        subject: 'Sorry to see you go!',
+        text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+    });
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
